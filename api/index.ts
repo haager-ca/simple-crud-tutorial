58bbeb0febab68a8f1dc5f4d392a361579967081
haager-ca <caius.haager@icloud.com>
2020-08-11 11:51:25 +0200

    added http verbs

--- api/index.ts
+++ api/index.ts
@@ -2,14 +2,23 @@ import * as express from 'express';
 
 const app = express();
 
-app.get("/", function (req, res) {
-    res.send("Hello world!")
+app.get("/users", function (req, res) {
+    res.send("Hier kommmen später die Benutzer raus!")
 });
 
-app.get("*", function (req, res) {
-    res.send("Diese Seite existiert nicht!")
+app.post("/users", function (req, res) {
+ res.send("Neuer Benutzer wurde angelegt.")
 });
+
+/*app.get("*", function (req, res) {
+    res.send("Diese Seite existiert nicht!")
+});*/
 
 app.listen(3000, function ()  {
  console.log("Server started on port 3000!");
 });
+
+/* GET -> Anzeige
+   POST -> Hinzufügen/ Aktualiesieren
+   DELET -> Löschen
+*/
