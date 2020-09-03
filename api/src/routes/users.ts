4ff8d5ffd6e13ab0eb17ebc8357fdcc5917b5747
haager-ca <caius.haager@icloud.com>
2020-09-03 22:20:41 +0200

    The frorgotten File

--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -2,23 +2,23 @@ import { Router } from "express";
 export const router = Router();
 
 
-router.get("/users", function (req, res) {
+router.get("/", function (req, res) {
     res.send("Hier kommmen später die Benutzer raus!")
 });
 
-router.get("/users/:id", function (req, res) {
+router.get("/:id", function (req, res) {
     res.send("Die Daten von einem Benutzer")
 });
 
-router.post("/users", function (req, res) {
+router.post("/", function (req, res) {
     res.send("Neuer Benutzer wurde angelegt.")
 });
 
-router.post("/users/:id", function (req, res) {
+router.post("/:id", function (req, res) {
     res.send("Der Benutzer wurde aktualisiert")
 });
 
-router.delete("/users/:id", function (req, res) {
+router.delete("/:id", function (req, res) {
     res.send("Der Benutzer wurde gelöscht")
 });
 
