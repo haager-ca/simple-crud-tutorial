5bac6e14ac4a508185399ccb7daec24235665ce6
haager-ca <caius.haager@icloud.com>
2020-09-06 12:15:06 +0200

    fixed an error in user.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,19 @@
 import * as express from "express";
 import routes from "./routes/index";
+import { createConnection } from "typeorm";
 
 const app = express();
 
 app.use("/api", routes);
+
+createConnection({
+    type: "mysql",
+    host: "localhost",
+    username: "root",
+    password: "",
+    database: "simpleCrudTutorialDB",
+})
+
 
 /*app.get("*", function (req, res) {
     res.send("Diese Seite existiert nicht!")
