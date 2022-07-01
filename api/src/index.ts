d840d98d8e83272105741be45da3fb5c3edaa6bf
haager-ca <caius.haager@icloud.com>
2022-07-01 16:01:46 +0200

    reinstall MySQL

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,27 +1,46 @@
 import * as express from "express";
 import routes from "./routes/index";
 import { createConnection } from "typeorm";
+import { User } from "./entities/user"
+import "reflect-metadata";
 
-const app = express();
 
-app.use("/api", routes);
-
-createConnection({
+const connection = createConnection({
     type: "mysql",
     host: "localhost",
     username: "root",
     password: "26ca.08HA.",
     database: "simplecrudtutorial",
+    entities: [User]
+}).then(async (connection) => {
+    
+    const app = express();
+
+
+    app.use("/api", routes);
+
+    app.listen(3000, function () {
+        console.log("Server started on port 3000!");
+    });
 })
+
+/* 
+
+    console.log("Connected");
+    await connection.synchronize();
+    console.log("Synchronized!");
+
+    const u = new User();
+    u.name = "cool";
+    connection.manager.save(u);
+*/
 
 
 /*app.get("*", function (req, res) {
     res.send("Diese Seite existiert nicht!")
 });*/
 
-app.listen(3000, function () {
-    console.log("Server started on port 3000!");
-});
+
 
 /* GET -> Anzeige
    POST -> Hinzufügen/ Aktualiesieren
